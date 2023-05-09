f91613744cb00719da787ed814da54b583850936
josephsmann <mannalytics@gmail.com>
2023-05-09 07:23:14 -0600

    changed __init__.py and cleaning

--- doubledot/__init__.py
+++ doubledot/__init__.py
@@ -1,1 +1,10 @@
 __version__ = "0.0.1"
+# from crema_sf import Salesforce
+# from ATMS_api import ATMS_api
+__all__ = ["Salesforce","ATMS_api"]
+
+# this is the magic syntax that allows you to import all the classes in the package
+from .crema_sf import Salesforce
+from .ATMS_api import ATMS_api 
+
+
