5530e810ee12e3d3a85fa9dcc4ee4ffa54b2a490
josephsmann <mannalytics@gmail.com>
2023-05-22 21:04:09 -0600

    verified lookups

--- doubledot/__init__.py
+++ doubledot/__init__.py
@@ -1,10 +1,10 @@
 __version__ = "0.0.1"
 # from crema_sf import Salesforce
 # from ATMS_api import ATMS_api
-__all__ = ["Salesforce","ATMS_api"]
+__all__ = ["Salesforce","ATMS"]
 
 # this is the magic syntax that allows you to import all the classes in the package
 from .crema_sf import Salesforce
-from .ATMS_api import ATMS_api 
+from .ATMS_api import ATMS_api as ATMS   
 
 
